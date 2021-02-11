94555d2d5e723050e947704603cbab6c9bce1073
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-02-11 21:12:44 +0900

    add comment tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -1,8 +1,10 @@
 #include "9cc.h"
 
+// 定義
 char *user_input;
 Token *token;
 
+// エラー報告
 void error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -11,6 +13,7 @@ void error(char *fmt, ...) {
   exit(1);
 }
 
+// 場所ありエラー報告
 void error_at(char *loc, char *fmt, ...)
 {
   va_list ap;
@@ -25,6 +28,7 @@ void error_at(char *loc, char *fmt, ...)
   exit(1);
 }
 
+// 与えられた文字列（ここでは現在のトークン）を用いて，与えられた文字数分の「文字列」を返す．
 char *duplicate(char *p, int len) {
   char *buf = malloc(len + 1);
   strncpy(buf, p, len);
@@ -32,17 +36,17 @@ char *duplicate(char *p, int len) {
   return buf;
 }
 
-// 次のトークンが期待している記号の時は，トークンを一つ進める．
-// それ以外の場合にはエラー処理をする．
+// 現在のトークンが「指定した記号」か確認．（トークン進めて，trueを返す）
 bool consume(char *op) {
   if (token->kind != TK_RESERVED ||
       strlen(op) != token->len ||
       memcmp(token->str, op, token->len))
     return false;
-  token = token->next; // トークンを一つ進める
+  token = token->next;
   return true;
 }
 
+// 現在のトークンが「ローカル変数」か確認．（トークン進めて，現在のトークン返す）
 Token *consume_ident() {
   if (token->kind != TK_IDENT)
     return NULL;
@@ -51,7 +55,7 @@ Token *consume_ident() {
   return t;
 }
 
-// consumeとちょっと違う動きするだけ
+// 現在のトークンが「指定した記号」か確認．
 void expect(char *op) {
   if (token->kind != TK_RESERVED ||
       strlen(op) != token->len ||
@@ -60,7 +64,7 @@ void expect(char *op) {
   token = token->next;
 }
 
-// tokenkindが数か確かめて，tokenを一つ進めて値を返す
+// 現在のトークンが「数」か確認．（トークン進めて，数返す）
 int expect_number() {
   if (token->kind != TK_NUM)
     error_at(token->str, "expected a number");
@@ -69,13 +73,14 @@ int expect_number() {
   return val;
 }
 
-// ===========================================================================
-
-// tokenkindが終端を指しているか確認
+// 現在のトークンが「終端」か確認．
 bool at_eof() {
   return token->kind == TK_EOF;
 }
 
+// ===================================================================================================================================
+
+// 新しいトークンを作成し，現在のトークンの「後方」に繋げる．（連結リスト）
 Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
   Token *tok = calloc(1, sizeof(Token));
   tok->kind = kind;
@@ -85,18 +90,22 @@ Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
   return tok;
 }
 
+// 与えられた文字列と，与えられた文字列が，等しいか確認．
 bool startswith(char *p, char *q) {
-  return memcmp(p, q, strlen(q)) == 0; // pとqを，strlen(q)文字分比較
+  return memcmp(p, q, strlen(q)) == 0; // memcmpは等しいと0を返す．
 }
 
+// 与えられた文字が「アルファベット」か確認
 bool is_alpha(char c) {
   return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
 }
 
+// 与えられた文字が「アルファベット」・「数字」か確認
 bool is_alnum(char c) {
   return is_alpha(c) || ('0' <= c && c <= '9');
 }
 
+// 与えられた文字列を，先頭から「トークンの連結リスト」に，変換．その後，先頭を返す．
 Token *tokenize() {
   char *p = user_input;
   Token head;
@@ -104,17 +113,21 @@ Token *tokenize() {
   Token *cur = &head;
 
   while (*p) {
+
+    // スペースはスキップ
     if (isspace(*p)) {
       p++;
       continue;
     }
 
+    // 「return」のトークンを作成．
     if (startswith(p, "return") && !is_alnum(p[6])) {
       cur = new_token(TK_RESERVED, cur, p, 6);
       p += 6;
       continue;
     }
 
+    // 「比較演算子」のトークンを作成．
     if (startswith(p, "==") ||
 	startswith(p, "!=") ||
 	startswith(p, "<=") ||
@@ -125,12 +138,13 @@ Token *tokenize() {
       continue;
     }
 
+    // 「単項演算子」のトークンを作成．
     if (strchr("+-*/()<>;=", *p)) {
       cur = new_token(TK_RESERVED, cur, p++, 1);
       continue;
     }
 
-    // identifier
+    // 「ローカル変数」のトークンを作成．
     if (is_alpha(*p)) {
       char *q = p++;
 
@@ -140,6 +154,7 @@ Token *tokenize() {
       continue;
     }
 
+    // 「数」のトークンを作成
     if (isdigit(*p)) {
       cur = new_token(TK_NUM, cur, p, 0);
       char *q = p;
@@ -151,6 +166,7 @@ Token *tokenize() {
     error_at(p, "invalid token");
   }
 
+  // 「終端」のトークンを作成．
   new_token(TK_EOF, cur, p, 0);
   return head.next;
 }
